0f439fa9a1b83973b631980e4ca85e88f27dc365
Sondre Nordjore <sondre.nordjore@vigoiks.no>
2022-12-22 14:47:22 +0100

    Initial commit

---
+++ src/main/java/no/fintlabs/FintorgeratorApplication.java
@@ -0,0 +1,13 @@
+package no.fintlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintorgeratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintorgeratorApplication.class, args);
+	}
+
+}
